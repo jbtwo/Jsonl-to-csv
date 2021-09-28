ef3d1f3a2e24c04c956fd785b0a6c9a2b5cd3f4b
jbtwo <justin.brown@shopify.com>
2021-09-28 16:07:07 -0400

    add header row, specific keys to parse from file

--- jsonl-to-csv.py
+++ jsonl-to-csv.py
@@ -1,11 +1,7 @@
-@author: Yasir hussain
-"""
-
-
 import glob
 import json
 import csv
-
+import re
 import time
 
 
@@ -14,18 +10,25 @@ start = time.time()
 from flatten_json import flatten
 
 #Path of jsonl file
-    File_path = 'path-to-folder-that-contains-jsonl-files'
+File_path = './data'
 #reading all jsonl files
-    files = [f for f in glob.glob( File_path + "**/*.jsonl", recursive=True)]
+files = [f for f in glob.glob( File_path + "**/*.jsonl", recursive=True)]
 i=0
+
+#write header row
+with open('output.csv', 'a' , newline='') as g:
+                writeheaders = csv.writer(g)
+                writeheaders.writerow(['id','createdAt','processedAt','updatedAt'])
 for f in files:
     with open(f, 'r') as F:
         for line in F:
 #flatten json files 
             data = json.loads(line)
             data_1=flatten(data)
+            normalize_id= re.search(r'\d+', data_1['id'])
+            data_1['id'] = normalize_id[0]
 #creating csv files  
-            with open('path-to-csv-file.csv', 'a' , newline='') as f:
+            with open('output.csv', 'a' , newline='') as f:
                 thewriter = csv.writer(f)
 #headers should be the Key values from json files that make Coulmn header
-                thewriter.writerow([data_1['header1'],data_1['header2']])
+                thewriter.writerow([data_1['id'],data_1['createdAt'],data_1['processedAt'],data_1['updatedAt']])
